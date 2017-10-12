9655b751ea6533927bb2b1b98724dbd639ef7a11
Joanna <joannag6@gmail.com>
2017-10-12 17:58:59 +1100

    Rows can be either sum or product, need to add this to puzzle_solver predicate

--- proj2.pl
+++ proj2.pl
@@ -2,28 +2,48 @@
 
 % puzzle_solution(Puzzle).
 
-puzzle_solution(Rows) :-
+puzzle_solution(Rows, RowElems, EqualsList, RowHeaders) :-
   Rows = [FirstRow | OtherRows], % OtherRows is a list of rows, incl headers
-  length(OtherRows, Length),
-  set_eq_list(Length, EqualsList), % makes a list of [#=] to be used in sum()
   transpose(Rows, Columns),
   Columns = [FirstCol | OtherCols], % OtherCols is a list of cols, incl headers
   maplist(separate_list, OtherRows, RowHeaders, RowElems), % RowElems are lists of row elem lists
   maplist(separate_list, OtherCols, ColHeaders, ColElems), % ColElems are lists of col elem lists
 
-  append(RowElems, Vs), Vs ins 1..9, % ensures all elems are between 1 to 9
+  % append(RowElems, Num), Num ins 1..9, % ensures all elems are between 1 to 9
+  maplist(all_distinct, RowElems), % ensure RowElems have distinct values in each row
+  maplist(all_distinct, ColElems). % ensure ColElems have distinct values in each col
 
-  maplist(sum, RowElems, EqualsList, RowHeaders), % ensure the sum of each row add up to the header
-  maplist(sum, ColElems, EqualsList, ColHeaders). % ensure the sum of each col add up to the header
+  % (maplist(sum, RowElems, EqualsList, RowHeaders); % ensure the sum of each row add up to the header
+  % maplist(product, RowElems, RowHeaders)),
+  % (maplist(sum, ColElems, EqualsList, ColHeaders); % ensure the sum of each col add up to the header
+  % maplist(product, ColElems, ColHeaders)).
 
 testSum([[0,4,6],[3,1,_],[7,_,4]]).
+
+
+
+% testPuzzle([[0,14,10,35],[14,_,_,_],[15,_,_,_],[28,_,1,_]]).
+testPuzzle([[0,14,10,35],[14,7,2,1],[15,3,7,5],[28,4,1,7]]).
 
 separate_list(Lst, Head, Tail) :-
   Lst = [Head | Tail].
 
-set_eq_list(Length, EqualsList) :-
-  length(EqualsList, Length),
-  maplist(=(#=), EqualsList).
+% set_eq_list(Length, EqualsList), % makes a list of [#=] to be used in sum()
+% set_eq_list(Length, EqualsList) :-
+%   length(EqualsList, Length),
+%   maplist(=(#=), EqualsList).
+
+% Recursive function to find product of a list.
+product(List, Product) :-
+  List = [Head | Tail],
+  product(Tail, NextProduct),
+  Product is Head * NextProduct.
+product([], Product) :-
+  Product = 1.
+
+% checks a row/col if they're sum or product.
+is_sum_or_prod(Elem, Header) :- product(Elem, Header),!. % for first check, if true, will check the second one too
+is_sum_or_prod(Elem, Header) :- sum(Elem, #=, Header).
 
 % Rows = [FirstRow | ElemRows], % ElemRows is a list of rows
 % FirstRow = [_ | ColHeaders], % ColHeaders is a list of column headers
